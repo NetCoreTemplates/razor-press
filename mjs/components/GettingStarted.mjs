07095fc78e7426c8d15119bfed7a8c11e85640b6
GitHub Action <action@github.com>
2023-10-29 13:23:47 +0000

    deploy: 8a47c60998fb0b673e11280e8cfeef90e8c6ef2e

--- mjs/components/GettingStarted.mjs
+++ mjs/components/GettingStarted.mjs
@@ -1,4 +1,4 @@
-import { ref } from "vue"
+import { ref, computed } from "vue"
 import ShellCommand from "./ShellCommand.mjs"
 
 export default {
@@ -12,6 +12,26 @@ export default {
       <input type="text" v-model="project" autocomplete="off" spellcheck="false" @keydown="validateSafeName"
              class="mb-8 sm:text-lg rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 dark:bg-gray-800"/>
 
+        <section class="w-full flex justify-center text-center">
+           <div class="mb-2">
+              <div class="flex justify-center text-center">
+                 <a class="archive-url hover:no-underline netcoretemplates_empty" :href="zipUrl('NetCoreTemplates/razor-press')">
+                    <div class="bg-white dark:bg-gray-800 px-4 py-4 mr-4 mb-4 rounded-lg shadow-lg text-center items-center justify-center hover:shadow-2xl dark:border-2 dark:border-pink-600 dark:hover:border-blue-600 dark:border-2 dark:border-pink-600 dark:hover:border-blue-600" style="min-width:150px">
+                       <div class="text-center font-extrabold flex items-center justify-center mb-2">
+                          <div class="text-4xl text-blue-400 my-3">
+                             <svg class="w-12 h-12 text-indigo-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0M3 6v13m9-13v13m9-13v13"></path></svg>
+                          </div>
+                       </div>
+                       <div class="text-xl font-medium text-gray-700">Razor Press</div>
+                       <div class="flex justify-center h-8"></div>
+                       <span class="archive-name px-4 pb-2 text-blue-600 dark:text-indigo-400">{{ projectZip }}</span>
+                       <div class="count mt-1 text-gray-400 text-sm"></div>
+                    </div>
+                 </a>
+              </div>
+           </div>
+        </section>
+
       <ShellCommand class="mb-2">dotnet tool install -g x</ShellCommand>
       <ShellCommand class="mb-2">x new {{template}} {{project}}</ShellCommand>
 
@@ -20,10 +40,26 @@ export default {
 
       <h4 class="py-6 text-center text-xl">Run .NET Project (New Terminal)</h4>
       <ShellCommand class="mb-2">dotnet watch</ShellCommand>
+
     </div>`,
     props: { template:String },
     setup(props) {
         const project = ref('ProjectName')
+
+        const projectZip = computed(() => (project.value || 'MyApp') + '.zip')
+
+        /** @param {string} template */
+        const zipUrl = (template) =>
+            `https://account.servicestack.net/archive/${template}?Name=${project.value || 'MyApp'}`
+
+        /** @param {KeyboardEvent} e */
+        const isAlphaNumeric = (e) => {
+            const c = e.charCode;
+            if (c >= 65 && c <= 90 || c >= 97 && c <= 122 || c >= 48 && c <= 57 || c === 95) //A-Za-z0-9_
+                return;
+            e.preventDefault()
+        }
+
         /** @param path {string}
           * @returns {string} */
         const resolvePath = (path) => navigator.userAgent.indexOf("Win") >= 0 ? path.replace(/\//g,'\\') : path
@@ -37,6 +73,6 @@ export default {
                 return false
             }
         }
-        return { project, uiPath, apiPath, validateSafeName }
+        return { project, projectZip, zipUrl, isAlphaNumeric, uiPath, apiPath, validateSafeName }
     }
 }
